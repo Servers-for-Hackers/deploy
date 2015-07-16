3d832a827b22c414c93648c1eea0256ee6cd4e5e
fideloper <fideloper@gmail.com>
2015-07-15 20:09:59 -0500

    updated ssh hostname in publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 rsync -vzcrSLh --exclude="deploy.sh" --exclude="design" --exclude=".git*" \
-    ./ sfh-deploy:/var/www/deploy.serversforhackers.com/public
+    ./ sfhdeploy:/var/www/deploy.serversforhackers.com/public
