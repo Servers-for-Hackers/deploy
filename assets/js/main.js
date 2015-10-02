337872fc3870cbfcdb0047be8fe55d4bc2b6ac6d
fideloper <fideloper@gmail.com>
2015-10-02 07:00:27 -0500

    styles for code and video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,90 +1,147 @@
 /*
-	Telephasic by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+ Telephasic by HTML5 UP
+ html5up.net | @n33co
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
+(function($,sr, sc, sk){
 
-(function($) {
+    // debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
 
-	skel.breakpoints({
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 1080px)',
-		narrower: '(max-width: 820px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
-	});
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, args);
+                timeout = null;
+            };
 
-	$(function() {
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
 
-		var	$window = $(window),
-			$body = $('body');
+            timeout = setTimeout(delayed, threshold || 100);
+        };
+    }
+    // smartresize
+    jQuery.fn[sr] = function(fn, thresh){  return fn ? this.bind('resize', debounce(fn, thresh)) : this.trigger(sr); };
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+    // smartchange
+    jQuery.fn[sc] = function(fn, thresh){  return fn ? this.bind('change', debounce(fn, thresh)) : this.trigger(sc); };
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+    // keyup
+    jQuery.fn[sk] = function(fn, thresh){  return fn ? this.bind('keyup', debounce(fn, thresh)) : this.trigger(sk); };
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+})(jQuery,'smartresize', 'smartchange', 'smartkeyup');
 
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
+(function ($) {
+    hljs.initHighlightingOnLoad();
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				mode: 'fade',
-				speed: 300,
-				alignment: 'center',
-				noOpenerFade: true
-			});
+    skel.breakpoints({
+        normal: '(max-width: 1280px)',
+        narrow: '(max-width: 1080px)',
+        narrower: '(max-width: 820px)',
+        mobile: '(max-width: 736px)',
+        mobilep: '(max-width: 480px)'
+    });
 
-		// Off-Canvas Navigation.
+    $(function () {
 
-		/*
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+        // Video resizing
+        _V_("vjs-video-current").ready(function () {
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							'<a href="index.html" class="link depth-0">Home</a>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'top',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
-		*/
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+            var myPlayer = this;
 
-	});
+            // Resizing
+            var aspectRatio = 641 / 1140;
+
+            function resizeVideoJS() {
+                // Get the parent element's actual width
+                var width = document.getElementById(myPlayer.id()).parentElement.offsetWidth;
+                // Set width to fill parent element, Set height
+                myPlayer.width(width).height(width * aspectRatio);
+            }
+
+            resizeVideoJS(); // Initialize the function
+            $(window).smartresize(function () {
+                resizeVideoJS();
+            });
+
+        });
+
+        var $window = $(window),
+            $body = $('body');
+
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
+
+        $window.on('load', function () {
+            $body.removeClass('is-loading');
+        });
+
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
+
+        // Prioritize "important" elements on narrower.
+        skel.on('+narrower -narrower', function () {
+            $.prioritize(
+                '.important\\28 narrower\\29',
+                skel.breakpoint('narrower').active
+            );
+        });
+
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9)
+            $(':last-child').addClass('last-child');
+
+        // Dropdowns.
+        $('#nav > ul').dropotron({
+            mode: 'fade',
+            speed: 300,
+            alignment: 'center',
+            noOpenerFade: true
+        });
+
+        // Off-Canvas Navigation.
+
+        /*
+         // Navigation Button.
+         $(
+         '<div id="navButton">' +
+         '<a href="#navPanel" class="toggle"></a>' +
+         '</div>'
+         )
+         .appendTo($body);
+
+         // Navigation Panel.
+         $(
+         '<div id="navPanel">' +
+         '<nav>' +
+         '<a href="index.html" class="link depth-0">Home</a>' +
+         $('#nav').navList() +
+         '</nav>' +
+         '</div>'
+         )
+         .appendTo($body)
+         .panel({
+         delay: 500,
+         hideOnClick: true,
+         resetScroll: true,
+         resetForms: true,
+         side: 'top',
+         target: $body,
+         visibleClass: 'navPanel-visible'
+         });
+         */
+        // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+            $('#navButton, #navPanel, #page-wrapper')
+                .css('transition', 'none');
+
+    });
 
 })(jQuery);
--- page.php
+++ page.php
@@ -3,16 +3,48 @@ require_once('vendor/autoload.php');
 
 $page = (isset($_GET['page'])) ? $_GET['page'] : false;
 
+$titles = [
+    '01-ssh-to-connect'      => 'SSH to Connect',
+    '02-new-users'           => 'New Users',
+    '03-ssh-security'        => 'SSH Security',
+    '04-firewalls'           => 'Firewalls',
+    '05-fail2ban'            => 'Fail2Ban',
+    '06-application-server'  => 'The Application Server',
+    '07-ssh-config'          => 'SSH Config',
+    '08-scp'                 => 'Deploying with SCP',
+    '09-rsync'               => 'Deploying with Rsync',
+    '09b-NEEDS-CREATING-SERVER-COMM-GIT' => 'Git &amp; Github',
+    '10-fabric-intro'        => 'Introducing Fabric',
+    '11-fabric-sudoers'      => 'Sudoers &amp; Sudo',
+    '12-fabric-symlinks'     => 'Downtime &amp; Symlinks',
+    '13-fabric-rollback'     => 'Rollbacks',
+    '14-fabric-migrations'   => 'Migrations',
+    '15-other-services'      => 'What else does this?',
+    '16-automate-id'         => 'Run Fabric Programmatically',
+    '17-create-build-server' => 'Creating a Build Server',
+    '18-web-listener'        => 'The Web Listener',
+    '19-github-webhook'      => 'Github Webhooks',
+    '20-setup-sqs'           => 'Using Queues with SQS',
+    '21-setup-python'        => 'Consuming Queue Jobs with Python',
+    '22-notify'              => 'Notifications During Deployment',
+    '23-upstart_systemd'     => 'Monitoring the Deploy Services',
+    '24-app-to-build-static' => 'Building the Application Ahead of Deployment',
+    '25-build-server-deps'   => 'Making the Build Server Build',
+    '26-deploy-zip'          => 'Deploying a Built Application',
+];
+
+$title = '';
 $html_content = '';
 
 if( $page )
 {
+    $title = (isset($titles[$page])) ? $titles[$page] : '';
     $html_content = Deploy\Render::render($page);
 }
 ?><!DOCTYPE HTML>
 <html>
 <head>
-    <title>Deploy! | Servers for Hackers</title>
+    <title><?=$title?> | Deploy!</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -38,7 +70,7 @@ if( $page )
     <meta name="twitter:domain" content="deploy.serversforhackers.com">
     <link rel="icon" href="/favicon.png">
 </head>
-<body class="homepage">
+<body class="content-page">
 <div id="page-wrapper">
 
     <!-- Header -->
@@ -52,14 +84,28 @@ if( $page )
     </div>
 
     <!-- One -->
-    <section class="wrapper style4 container">
+    <section class="page-content wrapper style4 container">
 
         <!-- Content -->
         <div class="content">
+            <article>
+                <header><h1><?=$title?></h1></header>
+            </article>
             <section>
-                <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                <?php echo $html_content; ?>
+            <div class="media-resize">
+                <video id="vjs-video-current" class="video-js vjs-default-skin vjs-big-play-centered"
+                       controls preload="auto" width="100%" height="641"
+                       poster="https://s3.amazonaws.com/serversforhackers/sfh-bumper-compressor.png">
+                    <source src="http://player.vimeo.com/external/118355954.hd.mp4?s=baa43f374fb1ed81fcca09524cdf818f" data-quality="hd" />
+                    <source src="http://player.vimeo.com/external/118355954.sd.mp4?s=740eacd4f71c4e9293a7c160941dc575" />
+                    <source src="http://player.vimeo.com/external/118355954.mobile.mp4?s=fc534961d7d3242aa71b6b9c2b0aaf28" />
+                    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                </video>
+            </div>
             </section>
+            <article>
+                <?php echo $html_content; ?>
+            </article>
         </div>
 
     </section>
@@ -78,12 +124,13 @@ if( $page )
 </div>
 
 <!-- Scripts -->
-
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/jquery.dropotron.min.js"></script>
 <script src="assets/js/skel.min.js"></script>
 <script src="assets/js/util.js"></script>
 <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
+<script src="//vjs.zencdn.net/4.12/video.js"></script>
 <script src="assets/js/main.js"></script>
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
